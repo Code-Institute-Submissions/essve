1dbdceb7dad0889a2bea39645a925e80fb1d63c8
Nicklas Jonsson <nicke.jonsson@live.com>
2020-06-18 08:34:34 +0200

    Added comments to the css and the js-files

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,9 @@
-function searchBar() {
+function searchBar() {  //JQuery function to hide the search-icon in the navbar and to replace it with a searchbar.
     $("#iconified").hide();
     $("#icons").html(`<input type="text" name="search" class="form-control searching" placeholder="Sök.."></input>`);
 };
 
-function backToNormal() {
+function backToNormal() {   //JQuery function to set it(above) back again on mouseleave.
     $("#icons").html("");
     $("#icons").html(`<i class="fa fa-search" id="iconified" onmouseenter="searchBar()" onmouseleave="backToNormal()"></i>`);
     $("#iconified").show();
--- assets/js/product.js
+++ assets/js/product.js
@@ -1,26 +1,26 @@
-function storeChoice(id){
-  sessionStorage.clear();
-  sessionStorage.setItem("menuChoice", id);  //stores the id of the menu-choice made by user.
+function storeChoice(id){ //Function beign called when user clicks on a category in the navbar.
+  sessionStorage.clear(); //Empties the sessionStorage
+  sessionStorage.setItem("menuChoice", id);  //stores the id of the menu-choice made by user, in a sessionStorage.
 }
 
-function writeToDocument(cb){ //The function begin called when the user makes a menu-choice.
+function writeToDocument(cb){ //Function onload of HTML-body of category-page.
 
-  var header = sessionStorage.getItem("menuChoice");
-  $(".category-header").html(header);
+  var header = sessionStorage.getItem("menuChoice");  //Gets the menu-choice made by user and stores it in the variable header.
+  $(".category-header").html(header); //Using JQuery to set the Category header.
 
-  getData(function(cb) {
-    data = cb.artiklar;
-    var tableRows = [];
+  getData(function(cb) {  //Calls the getData-function and passes in this function.
+    data = cb.artiklar; //Stores the response in data.
+    var tableRows = []; //Stores the filtered response.
 
-    for(var i = 0, len = data.length; i < len; i++) {
+    for(var i = 0, len = data.length; i < len; i++) { //A for-loop which loops through data.
       var choice = sessionStorage.getItem("menuChoice");
 
-      if (data[i].kategori === choice) {
-        var truncatedData = data[i];
-        tableRows.push(truncatedData);
+      if (data[i].kategori === choice) {  //If-statement that checks the value of the key "kategori" in data and compares it to the menu-choice made by user.
+        var truncatedData = data[i];  //Adds matched object to truncadedData.
+        tableRows.push(truncatedData);  //Pushes truncatedData to the array tableRows.
       }
     }
-    showData(tableRows);
+    showData(tableRows);  //Calls the showData-function and passes in tableRows
   });
 }
 
@@ -30,19 +30,19 @@ function getData(cb) {  //Creates the function getData
 
   req.onreadystatechange = function() {  //whenever the state changes of our xhr object, we want to run a check.
     if (req.readyState == XMLHttpRequest.DONE) {  //If the ready state is equal to 4 and the status is 200, then do the following:
-      cb(JSON.parse(req.responseText));  //cb calls the function getData to print the JSON-object to the console.
+      cb(JSON.parse(req.responseText));  //cb calls the function that we pass in when calling the getData-function. Before that it takes the API-response and parse it to JSON.
     }
   };
 
-req.open("GET", "https://api.jsonbin.io/b/5ee9f65619b60f7aa95bf20e", true);
-req.setRequestHeader("secret-key", "$2b$10$Z9Ndh/N6Bl69HrvPtVtZkefAv525x8CvixBbgTCg04AgcnenwY.oa");
+req.open("GET", "https://api.jsonbin.io/b/5ee9f65619b60f7aa95bf20e", true); //Opens the connection.
+req.setRequestHeader("secret-key", "$2b$10$Z9Ndh/N6Bl69HrvPtVtZkefAv525x8CvixBbgTCg04AgcnenwY.oa"); //The key to access the api.
 req.send();
 }
 
-function showData(tableRows) {
-  console.log(tableRows);
-  for(var i = 0, len = tableRows.length; i < len; i++){
-      $(".productPresent").append(`
+function showData(tableRows) {  //Declaration of the function ShowData. Which pushes the HTML below to the screen.
+  for(var i = 0, len = tableRows.length; i < len; i++){ //Runs for as many times as we have objects in our filtered array.
+    //Using JQuery below to get the element with the class of productPresent and appends the following HTML to that element.  
+    $(".productPresent").append(`
                   <div class="col-md-3">
                       <figure class="card card-product">
                           <button type="button" id="style-modal" data-toggle="modal" data-target="#productModal" onclick="storeProductChoice()">
